485f698a3ba13f6c70b0e744c78ff125d07f48bf
Joseph C B <josephcb581@gmail.com>
2024-10-15 22:40:06 +0530

    add dashboard homepage and css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from forms import SignUpForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +6,7 @@ from sqlalchemy import ForeignKey, String, Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 
 app = Flask(__name__)
 Bootstrap5(app)
@@ -19,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizzify.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "user"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), nullable=False)
@@ -28,6 +29,16 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
+
+# FLASK LOGIN
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
+
 #Routes
 
 @app.route("/")
@@ -45,7 +56,8 @@ def signup():
                             password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
-            return render_template("dashboard.html",name=new_user.name)
+            #login_user(new_user)
+            return redirect(url_for('dashboard'))
         else:
             flash("Account already exist. Please login")
     return render_template('signup.html', form=form)
@@ -58,12 +70,18 @@ def login():
         user = db.session.execute(db.select(User).where(User.email== form.email.data)).scalar()
         if user:
             if check_password_hash(user.password, typed_password):
-                return render_template("dashboard.html")
+                #login_user(user)
+                return redirect(url_for('dashboard'))
             else:
                 flash("Check your password and try againz.")
         else:
             flash("Email doesn't exist. Please sign up!")
     return render_template("login.html", form=form)
+
+@app.route('/dashboard',methods=["GET","POST"])
+def dashboard():
+    return render_template("dashboard.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
